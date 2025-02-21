b8acee7bd14b6ae82e2f366ee17fa920a15f85a2
Muyao Lu <130-MLU@sd43.bc.ca>
2025-02-20 20:37:03 -0800

    Delete music.js

--- music.js
+++
@@ -1,9 +0,0 @@
-const music_player = document.getElementById("music");
-function play_music(){
-    if (music_player.paused){
-        music_player.play()
-    }
-    else{
-        music_player.pause()
-    }
-}
